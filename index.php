82694fde2120a3bd2e222fa2322671f38db0e399
Eduardo Henrique <eduardo.horoutlook.com>
2020-05-11 17:07:36 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyId(4);
+//Carrega um Usuario
 
-echo $root;
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
+
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("ri");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha 
+$usuario = new Usuario();
+
+$usuario->login("Eduardo","qwert");
+
+echo $usuario;
+
 
  ?>
